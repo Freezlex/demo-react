d7e57e4c459762770ad34e1bed70755755ecc18b
Nathan.Engelmann <nathan.engelmann@uha.fr>
2021-01-05 10:36:52 +0100

    ajout des boutons de la page d'accueil

--- fnesi/screens/TabOneScreen.tsx
+++ fnesi/screens/TabOneScreen.tsx
@@ -1,32 +1,38 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { Container, Header, Content, Button, Text } from 'native-base';
+import {StyleSheet} from "react-native";
+export default class ButtonBlockExample extends Component {
+  render() {
+    return (
+        <Container style={styles.container}>
+          <Content>
+            <Button block style={styles.button}>
+              <Text>Jouer</Text>
+            </Button>
+            <Button block info style={styles.button}>
+              <Text>Créer une salle</Text>
+            </Button>
+            <Button block info style={styles.button}>
+              <Text>Rejoindre</Text>
+            </Button>
+            <Button block info style={styles.button}>
+              <Text>Partie rapide</Text>
+            </Button>
+          </Content>
+        </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
+  },
+  button: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    marginBottom:20,
+  }
 });
+
